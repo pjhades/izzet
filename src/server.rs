345071f9b88ca4abe3c7cb098bbcf609d3523b97
Jing Peng <pj.hades@gmail.com>
2017-11-13 20:22:23 -0500

    Request index.html if the URL points to a folder

--- src/server.rs
+++ src/server.rs
@@ -1,9 +1,15 @@
 use config::Config;
-use error::Result;
+use error::{Error, Result};
 use std::fs::File;
 use std::path::PathBuf;
 use std::str::FromStr;
-use tiny_http::{Header, Method, Response, Server, StatusCode};
+use tiny_http::{Header, Method, Request, Response, Server, StatusCode};
+
+fn resp_with_status(req: Request, code: u16) -> Result<()> {
+    println!("{} - {} {}", code, req.method().as_str(), req.url());
+    req.respond(Response::empty(StatusCode(code)))
+       .map_err(|e| Error::new(format!("fail to respond: {}", e)))
+}
 
 pub fn forever(dir: PathBuf, config: Config) -> Result<()> {
     let server = Server::http(("0.0.0.0", config.port.unwrap_or(::DEFAULT_PORT)))?;
@@ -11,18 +17,19 @@ pub fn forever(dir: PathBuf, config: Config) -> Result<()> {
     loop {
         let req = server.recv()?;
         if req.method() != &Method::Get {
-            req.respond(Response::empty(StatusCode(501)))?;
+            resp_with_status(req, 501)?;
             continue;
         }
 
         // skip the leading slash
-        let path = dir.join(&req.url()[1..]);
+        let mut path = dir.join(&req.url()[1..]);
+        if path.is_dir() {
+            path = path.join(::INDEX_FILE);
+        }
         match File::open(&path) {
-            Err(e) => {
-                eprintln!("fail to serve requested file {:?}: {}", &path, e);
-                req.respond(Response::empty(StatusCode(404)))?;
-            },
+            Err(_) => resp_with_status(req, 404)?,
             Ok(f) => {
+                println!("200 - {} {}", req.method().as_str(), req.url());
                 let resp = Response::from_file(f)
                     .with_header(Header::from_str("Cache-Control: no-cache,no-store,must-revalidate")?);
                 req.respond(resp)?;
